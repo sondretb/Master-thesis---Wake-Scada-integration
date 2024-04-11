e089adf67d94e8298424e141ead39d391d400758
Sondre <sondretb@stud.ntnu.no>
2024-04-11 12:16:34 +0200

    converted coord to meters

--- dudgeon.py
+++ dudgeon.py
@@ -4,15 +4,20 @@ from dotenv import load_dotenv
 import pandas as pd
 load_dotenv()
 
+
+"""
 conn_str = os.environ.get("CONNECTION_STRING")
 coordinates_sas = os.environ.get('COORDINATES_SAS')
 
 
 storage_service = BlobServiceClient.from_connection_string(conn_str=conn_str)
 blob_client = storage_service.get_blob_client(container='preliminarydata', blob='ntnu24data/Dudgeon_coordinates.csv')
+"""
 
+coordinates_sas = os.environ.get('COORDINATES_SAS')
 dataframe = pd.read_csv(coordinates_sas, sep=";")
 
-WT_X = dataframe['Longitude'].to_list()
-WT_Y = dataframe['Latitude'].to_list()
+
+WT_X = dataframe['Longitude'].to_numpy()*111111 # estimate
+WT_Y = dataframe['Latitude'].to_numpy()*111111  # estimate
 
